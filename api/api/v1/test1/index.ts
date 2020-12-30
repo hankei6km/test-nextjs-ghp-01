1aec158a232d8054080c0d8f9ecbe6da195063fc
hankei6km <gh-hankei6km@outlook.jp>
2020-12-30 15:56:20 +0900

    Use aspida-mock

--- api/api/v1/test1/index.ts
+++ api/api/v1/test1/index.ts
@@ -1,5 +1,7 @@
+import { mockMethods } from 'aspida-mock';
 import { Test1List, Test1Ids } from '../../../contentTypes';
 import { GetIdsQuery, GetTest1ItemQuery } from '../../../queryTypes';
+import { mockDataTest1 } from '../../../mockData';
 
 export type Methods = {
   get: {
@@ -16,3 +18,43 @@ export type Methods = {
     ];
   };
 };
+
+//  mock から polymorph を扱う方法が不明だったのでとりあえず.
+// mockMethods の return に polymorph を含めるても作用しなかった.
+type MockMethods = {
+  get: {
+    query: GetTest1ItemQuery | GetIdsQuery;
+    resBody: Test1List | Test1Ids;
+  };
+};
+
+// mock も production のコードに残る?
+export default mockMethods<MockMethods>({
+  get: ({ query }) => {
+    const mockDataTest1Ids = {
+      ...mockDataTest1,
+      contents: mockDataTest1.contents.map(({ id }) => ({ id }))
+    };
+    const mockDataTest1List = {
+      ...mockDataTest1,
+      contents: mockDataTest1.contents.map((v) => ({
+        ...v,
+        content: undefined
+      }))
+    };
+
+    return {
+      status: 200,
+      resHeaders: {},
+      resBody: (() => {
+        switch (query.fields) {
+          case 'id':
+            return mockDataTest1Ids;
+          case 'id,title':
+            return mockDataTest1List;
+        }
+        return mockDataTest1;
+      })()
+    };
+  }
+});
