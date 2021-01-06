8ce0d71972d219a72f64141f8c5cac9d9e4c8a82
hankei6km <gh-hankei6km@outlook.jp>
2021-01-06 15:47:38 +0900

    Test Iamge.onLoad

--- components/ThumbImage.test.tsx
+++ components/ThumbImage.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
+// import { act } from 'react-test-renderer';
 
 import ThumbImage from './ThumbImage';
 
@@ -10,34 +11,50 @@ afterEach(() => {
   global.Image = orgImage;
 });
 
-function mockImage(
-  addEventListener = jest.fn(),
-  removeEventListener = jest.fn()
-) {
-  // mockImplementation だと mock.instances でプロパティの値が参照できなかった
-  // そういう風に使うものではない?
-  const image = jest.fn();
-  image.prototype.addEventListener = addEventListener;
-  image.prototype.removeEventListener = removeEventListener;
-  return image;
-}
-
 describe('ThumbImage', () => {
-  it('should not rendered img before Image.onLoad', () => {
-    global.Image = mockImage();
-    const { container } = render(
-      <ThumbImage
-        src="/path/to/image.png"
-        alt="thumbIamge"
-        thumbWidth={400}
-        thumbHeight={100}
-      />
-    );
-    const img = container.querySelector('img');
-    expect(img).not.toBeInTheDocument();
+  it('should renders img after Image.onLoad', async () => {
+    const addEventListener = jest.fn();
+    global.Image = jest.fn().mockImplementation((w: number, h: number) => {
+      const image = new orgImage(w, h);
+      Object.defineProperty(image, 'src', {
+        set: (v) => {},
+        get: () => undefined
+      });
+      image.addEventListener = addEventListener;
+      return image;
+    });
+
+    await act(async () => {
+      const { container } = render(
+        <ThumbImage
+          src="/path/to/image.png"
+          alt="thumbIamge"
+          thumbWidth={400}
+          thumbHeight={100}
+        />
+      );
+      await new Promise((resolve) => {
+        const img = container.querySelector('img');
+        expect(img).not.toBeInTheDocument();
+        setTimeout(() => {
+          // useEEffect 待ち、タイミング依存
+          addEventListener.mock.calls[0][1]();
+          const img = container.querySelector('img');
+          expect(img).toBeInTheDocument();
+          expect(addEventListener).toHaveBeenCalled();
+          resolve();
+        }, 10);
+        // resolve();
+      });
+    });
   });
+
   it('should be set thumbnail attributes into contained Image', () => {
-    const image = mockImage();
+    // mockImplementation だと mock.instances でプロパティの値が参照できなかった
+    // そういう風に使うものではない?
+    const image = jest.fn();
+    image.prototype.addEventListener = jest.fn();
+    image.prototype.removeEventListener = jest.fn();
     global.Image = image;
     render(
       <ThumbImage
