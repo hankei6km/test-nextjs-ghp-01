9b387b58b490e3e5e7c216bfebb72bfceba560c8
hankei6km <gh-hankei6km@outlook.jp>
2021-01-20 12:50:07 +0900

    Make SectionContent component

--- components/SectionItem.tsx
+++ components/SectionItem.tsx
@@ -2,16 +2,10 @@ import React, { useContext, ElementType } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Typography, { TypographyProps } from '@material-ui/core/Typography';
-import ArticleList from './ArticleList';
 import { Section as SectionType } from '../types/pageTypes';
 import { pruneClasses } from '../utils/classes';
 import SectionContext from './SectionContext';
-import SiteTitle from './parts/SiteTitle';
-import SiteLogo from './parts/SiteLogo';
-import PageTitle from './parts/PageTitle';
-import ProfileImage from './parts/ProfileImage';
-import NavMain from './parts/NavMain';
-import DateUpdated from './parts/DateUpdated';
+import SectionContent from './SectionContent';
 
 const useStyles = makeStyles(() => ({
   'SectionItem-root': {},
@@ -55,50 +49,11 @@ const SectionItem = ({ data, classes: inClasses }: Props) => {
         </Typography>
       )}
       {data.content.map((content, i) => (
-        <Box key={i}>
-          {content.kind === 'html' && (
-            <Box
-              className={classes['SectionItem-contentBody']}
-              dangerouslySetInnerHTML={{ __html: content.contentHtml }}
-            ></Box>
-          )}
-          {content.kind === 'partsSiteTitle' && (
-            <SiteTitle link={content.link} classes={{ ...inClasses }} />
-          )}
-          {content.kind === 'partsSiteLogo' && (
-            <SiteLogo
-              size={content.size}
-              link={content.link}
-              classes={{ ...inClasses }}
-            />
-          )}
-          {content.kind === 'partsPageTitle' && (
-            <PageTitle link={content.link} classes={{ ...inClasses }} />
-          )}
-          {content.kind === 'partsProfileImage' && (
-            <ProfileImage
-              size={content.size}
-              name={content.name}
-              link={content.link}
-              classes={{ ...inClasses }}
-            />
-          )}
-          {content.kind === 'partsUpdated' && (
-            <DateUpdated classes={{ ...inClasses }} />
-          )}
-          {content.kind === 'partsNavMain' && (
-            <NavMain classes={{ ...inClasses }} />
-          )}
-          {content.kind === 'posts' && (
-            <Box className={classes['SectionItem-contentArticles']}>
-              <ArticleList
-                items={content.contents}
-                detail={content.detail}
-                classes={{ ...inClasses }}
-              />
-            </Box>
-          )}
-        </Box>
+        <SectionContent
+          key={`${i}-${content.kind}`}
+          content={content}
+          classes={{ ...inClasses }}
+        />
       ))}
     </>
   );
