b2e2d9a8702978dec1406e5ead01dc09226e9df4
hankei6km <gh-hankei6km@outlook.jp>
2021-02-12 12:25:26 +0900

    Use notistack
    メッセージ表示の構成見直しで、これも暫定的なコミット

--- components/parts/MessageBanner.tsx
+++ components/parts/MessageBanner.tsx
@@ -1,36 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-import Snackbar from '@material-ui/core/Snackbar';
-import Typography from '@material-ui/core/Typography';
-import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import Alert from '@material-ui/lab/Alert';
 import CloseIcon from '@material-ui/icons/Close';
+import { useSnackbar } from 'notistack';
 import { pruneClasses } from '../../utils/classes';
 
 // Snackbar だと複数表示が面倒なので作り直す予定
 
 const useStyles = makeStyles(() => ({
-  'MessageBanner-root': {
-    top: 0,
-    width: '100%'
-  },
-  'MessageBanner-alert': {
-    width: '100%'
-  },
-  'MessageBanner-close': {
-    paddingTop: 0,
-    paddingBottom: 0
-  }
+  // _app.tsx で SnackbarProvider の className を上書きすることで
+  // container 側の幅を 100% まで広げている.
+  'MessageBanner-message': { width: '100%', alignSelf: 'center' },
+  'MessageBanner-close': { paddingTop: 0, paddingBottom: 0 }
 }));
-const classNames = [
-  'MessageBanner-root',
-  'MessageBanner-alert',
-  'MessageBanner-close'
-];
-
-function Alert(props: AlertProps) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
+const classNames = ['MessageBanner-message', 'MessageBanner-close'];
 
 type Props = {
   message: string;
@@ -46,35 +30,58 @@ const MessageBanner = ({
   classes: inClasses
 }: Props) => {
   const classes = useStyles({ classes: pruneClasses(inClasses, classNames) });
-  const [open, setOpen] = useState(true);
-  const severity =
-    variant === 'info' ? 'info' : variant === 'warning' ? 'warning' : 'error';
-  return (
-    <Snackbar
-      open={open}
-      onClose={(_e, reason) => reason === 'timeout' && setOpen(false)}
-      autoHideDuration={autoHide ? 10 * 1000 : null}
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      className={classes['MessageBanner-root']}
-    >
-      <Alert
-        severity={severity}
-        className={classes['MessageBanner-alert']}
-        action={
-          <IconButton
-            aria-label="close"
-            color="inherit"
-            className={classes['MessageBanner-close']}
-            onClick={() => setOpen(false)}
-          >
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        }
-      >
-        <Typography variant="body1"> {`${message}`}</Typography>
-      </Alert>
-    </Snackbar>
-  );
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  useEffect(() => {
+    enqueueSnackbar(message, {
+      preventDuplicate: true,
+      persist: !autoHide,
+      // variant:
+      //   variant === 'info'
+      //     ? 'info'
+      //     : variant === 'warning'
+      //     ? 'warning'
+      //     : 'error',
+      // dense: true,
+      //hideIconVariant: true,
+      anchorOrigin: {
+        vertical: 'top',
+        horizontal: 'center'
+      },
+      // content: (key, message) => (
+      //   <div id={key} className={classes['MessageBanner-message']}>
+      //     {message}
+      //   </div>
+      // ),
+      content: (key, message) => (
+        <Alert
+          id={`${key}`}
+          elevation={6}
+          variant="filled"
+          severity={
+            variant === 'info'
+              ? 'info'
+              : variant === 'warning'
+              ? 'warning'
+              : 'error'
+          }
+          className={classes['MessageBanner-message']}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              className={classes['MessageBanner-close']}
+              onClick={() => closeSnackbar(key)}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+        >
+          {message}
+        </Alert>
+      )
+    });
+  }, [enqueueSnackbar, closeSnackbar, message, autoHide, variant, classes]);
+  return <></>;
 };
 
 export default MessageBanner;
